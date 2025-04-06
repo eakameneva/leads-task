8a7b0bef5699851415ae20b47bf85bc9460fcb1e
eakameneva <kate103422@gmail.com>
2025-04-06 16:46:33 +0200

    feat: implement recipe fetching

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log('Hi');
+async function getRandomRecipe() {
+  try {
+    const response = await fetch(
+      'https://www.themealdb.com/api/json/v1/1/random.php'
+    );
+    if (!response.ok) {
+      throw new Error('Could not fetch a recipe');
+    }
+
+    const data = await response.json();
+  } catch (error) {
+    console.error('Error loading recipe:', error);
+  }
+}
+
+getRandomRecipe();
